3033130468566c9c04a91bef88496fbf8e4f39c3
Noel Hack <Noel.hack12@gmail.com>
2024-11-27 13:52:12 +0100

    i changed the data set and updated the main now i am satisfied

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 import imutils
 
 # Load the image
-img = cv2.imread("Example_pics/pic/9a4fe6361597f264.jpg")
+img = cv2.imread("archive/images/Cars309.png")
 if img is None:
     print("Error: Image not found. Check the file path.")
     exit()
@@ -18,7 +18,7 @@ plt.show()
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Apply noise reduction
-bfilter = cv2.bilateralFilter(gray, 11, 20, 20)
+bfilter = cv2.bilateralFilter(gray, 4, 20, 20)
 plt.imshow(bfilter, cmap='gray')
 plt.title('Noise Reduced Image')
 plt.show()
@@ -30,13 +30,21 @@ plt.imshow(edged, cmap='gray')
 plt.title('Edge Detection')
 plt.show()
 
+kernel = np.ones((2, 2), np.uint8) 
+dilated_image = cv2.dilate(edged, kernel, iterations=1)
+
+plt.imshow(dilated_image, cmap='gray')
+plt.title('Edges dialated')
+plt.show()
+
 # Find contours
-keypoints = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+keypoints = cv2.findContours(dilated_image.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 contours = imutils.grab_contours(keypoints)
 contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
 
 # Loop through contours to find a 4-point contour
-min_area = 1000  # You can tweak this threshold
+min_area = 900  # You can tweak this threshold
+location = None
 for contour in contours:
     if cv2.contourArea(contour) > min_area:
         approx = cv2.approxPolyDP(contour, 0.04 * cv2.arcLength(contour, True), True)
@@ -63,17 +71,6 @@ cv2.drawContours(mask, [location], -1, 255, -1)
 new_image = cv2.bitwise_and(img, img, mask=mask)
 plt.imshow(cv2.cvtColor(new_image, cv2.COLOR_BGR2RGB))
 plt.title('Masked Image')
-plt.show()
-
-# Crop the region of interest
-(x, y) = np.where(mask == 255)
-(x1, y1) = (np.min(x), np.min(y))
-(x2, y2) = (np.max(x), np.max(y))
-cropped_image = gray[x1:x2+1, y1:y2+1]
-
-# Display cropped image
-plt.imshow(cropped_image, cmap='gray')
-plt.title('Cropped Image')
 plt.show()
 
 
